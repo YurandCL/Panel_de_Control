6c48c0b94929c720dbd08df20b43fd559bf414f7
YurandCL <yurand.coloma@tecsup.edu.pe>
2019-01-02 18:10:37 -0500

    Consulta hacia la base de datos completa con nombres y dias de adeudo

--- APILobo/app/Http/Controllers/EmpresasController.php
+++ APILobo/app/Http/Controllers/EmpresasController.php
@@ -14,47 +14,9 @@ class EmpresasController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function getIndex(){
-        $factura_vencida = DB::table('tbl_factura')->select('factura_cod','factura_fvencimiento')->where('factura_estado', '=', '0')->get();
-        //codigo de la factura emitida
-        $arreglo_facturas_cod = array();
-        //fecha de la factura emitida
-        $arreglo_facturas_fvencimiento = array();
-        
-        //obtenemos todos los codigos de las empresas
-        for ($i=0; $i < sizeof($factura_vencida); $i++) { 
-             array_push($arreglo_facturas_cod, $factura_vencida[$i]->factura_cod);
-         }
-
-        //obtenemos la fecha de vencimiento de cada una de las facturas
-        for ($i=0; $i < sizeof($factura_vencida); $i++) { 
-             array_push($arreglo_facturas_fvencimiento, $factura_vencida[$i]->factura_fvencimiento);
-         }
-        
-        //obtenemos el codigo de todas las empresas que nos deben
-        $arreglo_empresas = array();
-        $codigo=0;
-        
-        for ($i=0; $i < sizeof($arreglo_facturas_cod); $i++){
-
-            $codigo_empresa = DB::table('tbl_factura')->select('factura_cliente_cod')->where('factura_cod', '=', $arreglo_facturas_cod[$i])->first();
-            foreach ($codigo_empresa as $key => $value) {
-               $codigo = $value;
-
-               //obtenemos el nombre de la empresa con el codigo que obtenemos antes
-               $nombre_empresa = DB::table('tbl_empresa')->select('empresa_razonsocial')->where('empresa_cod', '=', $codigo)->first();
-               array_push($arreglo_empresas, $nombre_empresa->empresa_razonsocial);
-            }
-        }
-       
-        return $arreglo_empresas;
-    }
-
-    public function recorrido_array($arreglos){
-        foreach ($arreglos as $key => $value) {
-           $valor_final = $value;
-       }
-        return $valor_final;
+    public function getListado(){
+        $test = DB::select('SELECT e.empresa_razonsocial, (CAST(now() as date) - f.factura_fvencimiento) as calc from tbl_empresa as e inner join tbl_factura as f on e.empresa_cod = f.factura_cliente_cod where f.factura_estado = ?', [0]);
+          return $test;
     }
 
     public function create()
