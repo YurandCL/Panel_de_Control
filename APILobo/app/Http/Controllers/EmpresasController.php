f581165b5feb739115fbbff993615e71f7979544
YurandCL <yurand.coloma@tecsup.edu.pe>
2019-01-11 14:21:15 -0500

    arreglo en el contenido de la funcion, estetico

--- APILobo/app/Http/Controllers/EmpresasController.php
+++ APILobo/app/Http/Controllers/EmpresasController.php
@@ -3,14 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-//use Illuminate\Support\Facades\DB;
 use DB;
 use App\Http\Requests;
 
 class EmpresasController extends Controller
 {
     public function getListado(){
-        $test = DB::select('SELECT f.factura_numero, e.empresa_razonsocial, (CAST(now() as date) - f.factura_fvencimiento) as calc from tbl_empresa as e inner join tbl_factura as f on e.empresa_cod = f.factura_cliente_cod where f.factura_estado = ? order by calc desc', [0]);
+        $test = DB::select('SELECT f.factura_numero, e.empresa_razonsocial,
+          (CAST(now() as date) - f.factura_fvencimiento) as calc
+          from tbl_empresa as e inner join tbl_factura as f
+          on e.empresa_cod = f.factura_cliente_cod where f.factura_estado = ?
+          order by calc desc', [0]);
           return $test;
     }
 }
